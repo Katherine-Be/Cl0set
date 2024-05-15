75e6c810a4e5149ac5b336bb5f01df712843e68c
Katherine Bensley <katherine.bensley.93@gmail.comm>
2024-05-14 20:00:23 -0400

    change table name

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,10 @@ CREATE DATABASE userInventory_db;
 -- use inventory_db database --
 USE userInventory_db;
 
-CREATE TABLE shoes (
+CREATE TABLE users (
     id INT NOT NULL, /* need to change to auto-increment*/
-    color VARCHAR(20) NOT NULL,
-    style VARCHAR(20)
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
 );
 
 
